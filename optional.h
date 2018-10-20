9a3c09342eea51755d76b534b2967b09612ce658
fr0streaper <andrei1999kashukov@gmail.com>
2018-10-20 17:50:07 +0300

    Remove is_empty; Move implementation from optional.cpp to optional.h

--- optional.h
+++ optional.h
@@ -5,32 +5,91 @@
 #ifndef OPTIONAL_OPTIONAL_H
 #define OPTIONAL_OPTIONAL_H
 
+#include <utility>
 #include <memory>
 
 template <typename T>
-struct optional
-{
+struct optional {
 
-    optional();
+    optional() {}
 
-    optional(T const&);
+    optional(T const &_value)
+    {
+        value = std::unique_ptr<T>(new T(_value));
+    }
 
-    optional(optional<T> const&);
+    optional(optional<T> const &rhs)
+    {
+        if (rhs)
+        {
+            value = std::unique_ptr<T>(new T(*rhs.value));
+        }
+        else if (value)
+        {
+            value.reset();
+        }
+    }
 
-    optional<T>& operator=(optional<T> const&);
+    optional<T> &operator=(optional<T> const &rhs)
+    {
+        if (rhs)
+        {
+            value = std::unique_ptr<T>(new T(*rhs.value));
+        }
+        else if (value)
+        {
+            value.reset();
+        }
 
-    explicit operator bool() const;
+        return *this;
+    }
 
-    T& operator*();
-    T const& operator*() const;
+    explicit operator bool() const
+    {
+        return static_cast<bool>(value);
+    }
 
-    T* operator->();
-    T const* operator->() const;
+    T &operator*()
+    {
+        if (!value)
+        {
+            throw std::runtime_error("Runtime error: dereference of empty optional object");
+        }
+        return (*value);
+    }
+    T const& operator*() const
+    {
+        if (!value)
+        {
+            throw std::runtime_error("Runtime error: dereference of empty optional object");
+        }
+        return (*value);
+    }
 
-    void clear();
+    T* operator->()
+    {
+        if (!value)
+        {
+            throw std::runtime_error("Runtime error: dereference of empty optional object");
+        }
+        return value.get();
+    }
+    T const* operator->() const
+    {
+        if (!value)
+        {
+            throw std::runtime_error("Runtime error: dereference of empty optional object");
+        }
+        return value.get();
+    }
+
+    void clear()
+    {
+        value.reset();
+    }
 
     template <typename U>
-    friend void swap(optional<U>&, optional<U>&);
+    friend void swap(optional<U>& a, optional<U>& b);
 
     template <typename U>
     friend decltype((std::declval<U>() < std::declval<U>()), bool())
@@ -58,10 +117,114 @@ struct optional
 
 private:
 
-    bool is_empty;
     std::unique_ptr<T> value;
 
 };
+
+template <typename U>
+void swap(optional<U>& a, optional<U>& b)
+{
+    if (a && b)
+    {
+        swap(a.value, b.value);
+    }
+    else if (a)
+    {
+        b = a;
+        a.clear();
+    }
+    else if (b)
+    {
+        a = b;
+        b.clear();
+    }
+}
+
+template <typename U>
+decltype((std::declval<U>() < std::declval<U>()), bool())
+operator<(optional<U> const& a, optional<U> const& b)
+{
+    if (!b)
+    {
+        return false;
+    }
+    if (!a)
+    {
+        return true;
+    }
+
+    return *a.value < *b.value;
+}
+
+template <typename U>
+decltype((std::declval<U>() <= std::declval<U>()), bool())
+operator<=(optional<U> const& a, optional<U> const& b)
+{
+    if (!a)
+    {
+        return true;
+    }
+    if (!b)
+    {
+        return false;
+    }
+
+    return *a.value <= *b.value;
+}
+
+template <typename U>
+decltype((std::declval<U>() == std::declval<U>()), bool())
+operator==(optional<U> const& a, optional<U> const& b)
+{
+    if (static_cast<bool>(a) != static_cast<bool>(b))
+    {
+        return false;
+    }
+    if (!a)
+    {
+        return true;
+    }
+    return *a.value == *b.value;
+}
+
+template <typename U>
+decltype((std::declval<U>() == std::declval<U>()), bool())
+operator!=(optional<U> const& a, optional<U> const& b)
+{
+    return !(a == b);
+}
+
+template <typename U>
+decltype((std::declval<U>() >= std::declval<U>()), bool())
+operator>=(optional<U> const& a, optional<U> const& b)
+{
+    if (!b)
+    {
+        return true;
+    }
+    if (!a)
+    {
+        return false;
+    }
+
+    return *a.value >= *b.value;
+}
+
+template <typename U>
+decltype((std::declval<U>() > std::declval<U>()), bool())
+operator>(optional<U> const& a, optional<U> const& b)
+{
+    if (!a)
+    {
+        return false;
+    }
+    if (!b)
+    {
+        return true;
+    }
+
+    return *a.value > *b.value;
+}
 
 
 #endif //OPTIONAL_OPTIONAL_H
